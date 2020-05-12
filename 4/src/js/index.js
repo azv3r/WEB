703cf8b1c5dfaa41c461ad897a7a9a380e5a58f2
azv3r <azv3r@users.noreply.github.com>
2020-05-12 19:14:30 +0500

    jigglypuff
    +jigglypuff

--- 4/src/js/index.js
+++ 4/src/js/index.js
@@ -1,10 +1,13 @@
 import { home } from './home';
 import { pokemon } from './pokemon';
+import { jigglypuff } from './jigglypuff.js'
 
 let btnHome = document.querySelector('.home'),
     btnPokemon = document.querySelector('.pokemon'),
+    btnJiggly = document.querySelector('.jigglypuff'),
     app = document.querySelector('#app')
 
 home()
 btnHome.onclick = home
 btnPokemon.onclick = pokemon
+btnJiggly.onclick = jigglypuff
